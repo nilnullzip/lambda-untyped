dadb1b208794da2406d6b9870d46400ec412dc31
Juan Pineda <juan@logician.com>
2014-02-19 22:45:18 +0000

    Define expression. Beginning of parsing.

--- src/Main.hs
+++ src/Main.hs
@@ -6,3 +6,29 @@ main :: IO ()
 main = do
     putStrLn "Welcome to FP Haskell Center!"
     putStrLn "Have a good day!"
+
+-- The syntax tree
+
+data Variable = Variable Char
+  deriving Show
+
+data Expression = Terminal Variable
+    | Lambda Variable Expression
+    | Application Expression Expression
+  deriving Show
+
+-- Parse expression
+
+parse :: String -> Expression
+
+-- Lambda:
+
+parse ('L':x:'.':e) = Lambda (Variable x) (parse e)
+
+-- Terminal
+
+parse (x:[]) = Terminal (Variable x)
+
+-- Application
+
+-- parse (x:e)

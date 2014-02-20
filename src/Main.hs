2b6b123ae695373f8abfc5d2bb8af0a59b2799b9
Juan Pineda <juan@logician.com>
2014-02-20 11:29:07 +0000

    Work on eval.

--- src/Main.hs
+++ src/Main.hs
@@ -6,28 +6,29 @@ module Main where
 import Debug.Trace
 import Data.Char
 import System.Exit
+import qualified Data.Map as Map
 
 -- | The Lambda expression to parse
 
 identity = "Lz.z"
 zero = "Ls.Lz.z"
-one  = "Ls.Lz.s(z)"
-two  = "Ls.Lz.s(s(z))"
+one  = "Ls.Lz.sz"
+two  = "Ls.Lz.s(sz)"
 successor = "Lw.Ly.Lx.y(wyx)"
 
---theLambda = "Lx.xyzLa.ab"
-theLambda = "S"
+theLambda = "(Lx.x)y"
 
 -- | The main entry point.
 main :: IO ()
 main = do
     putStrLn "Juan's Lambda calculus interpreter!"
     print (parse theLambda)
+    print (eval (parse theLambda) env)
 
 -- | The syntax tree
 
 data Expr =
-    Var Char
+      Var Char
     | Lambda Char Expr
     | Apply Expr Expr
   deriving (Show, Eq)
@@ -45,8 +46,8 @@ parse s
 parseApply :: Expr -> String -> Expr
 
 parseApply acc s
-    | s == "" = trace "parseLeft: EOS" acc
-    | otherwise = trace "parseLeft: Apply" (parseApply (Apply acc t1) r1)
+    | s == "" = {- trace "parseLeft: EOS" -} acc
+    | otherwise = {- trace "parseLeft: Apply" -} (parseApply (Apply acc t1) r1)
     where (t1,r1) = parseTerm s
 
 -- | Parse a term
@@ -72,8 +73,25 @@ parseTerm (x:r) | (isLower x) = (Var x, r)
 
 parseTerm (x:r) | x == 'I' = (parse(identity), r)
 parseTerm (x:r) | x == '0' = (parse(zero), r)
+parseTerm (x:r) | x == '1' = (parse(one), r)
+parseTerm (x:r) | x == '2' = (parse(two), r)
 parseTerm (x:r) | x == 'S' = (parse(successor), r)
 
 -- | Syntax error:
 
 parseTerm(s) = error ("parseTerm: syntax error: " ++ s)
+
+-- | Evaluate
+
+env = Map.insert 1 2 Map.empty
+
+weval :: Expr -> fromList -> Expr
+weval (Var x) env = {- trace ("eval: " ++ show x) -} (Var x)
+weval (Lambda x e) env = {- trace ("eval: lambda " ++ show x) -} (Lambda x (weval e env))
+weval (Apply a b) env = {- trace ("eval: apply") -} (Apply (weval a env) (weval b env))
+
+
+eval :: Expr -> fromList -> Expr
+eval (Var x) env = {- trace ("eval: " ++ show x) -} (Var x)
+eval (Lambda x e) env = {- trace ("eval: lambda " ++ show x) -} (Lambda x (eval e env))
+eval (Apply (Lambda x e) a) env = {- trace ("eval: apply") -} (Apply (Lambda x e) (eval a env))

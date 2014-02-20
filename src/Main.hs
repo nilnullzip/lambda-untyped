ec1eafbdbd78a87a3e4eb2a12199701481bffea9
Juan Pineda <juan@logician.com>
2014-02-20 06:43:09 +0000

    Parsing works. Adding syntax checking.

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,67 @@
 -- | Main entry point to the application.
 module Main where
 
+import Debug.Trace
+import Data.Char
+
+
+-- foo :: String -> String
+-- foo "x" = "foo"
+
+data Foo = A | B
+    deriving (Show, Eq)
+
+x = (A == B)
+
 -- | The main entry point.
 main :: IO ()
 main = do
-    putStrLn "Welcome to FP Haskell Center!"
-    putStrLn "Have a good day!"
+    putStrLn "Juan's Lambda calculus interpreter!"
+    print (parseLeft EmptyExpression "Lx.Xx")
 
 -- The syntax tree
 
 data Variable = Variable Char
-  deriving Show
+  deriving (Show, Eq)
 
-data Expression = Terminal Variable
+data Expression =
+    Terminal Variable
     | Lambda Variable Expression
     | Application Expression Expression
-  deriving Show
+    | EmptyExpression
+  deriving (Show, Eq)
 
--- Parse expression
+-- Parse expression and return syntax tree
 
 parse :: String -> Expression
+parse s = parseLeft EmptyExpression s
+
+-- Left associative expression list
+
+parseLeft :: Expression -> String -> Expression
+
+parseLeft acc s
+    | s == "" = trace "parseLeft: EOS" acc
+    | acc == EmptyExpression = trace "parseLeft: Begin" (parseLeft t1 r1)
+    | otherwise = trace "parseLeft: Application" (parseLeft (Application acc t1) r1)
+    where (t1,r1) = parseOne s
+
+-- Parse one term
+
+parseOne :: String -> (Expression, String)
+
+parseOne ('(':s) =
+    let (e,r) = break (== ')') s in
+    ((parse e) , r)
 
 -- Lambda:
 
-parse ('L':x:'.':e) = Lambda (Variable x) (parse e)
+parseOne ('L':x:'.':e) | (isLower x) = (Lambda (Variable x) (parse e), "")
 
--- Terminal
+-- Terminal symbol
 
-parse (x:[]) = Terminal (Variable x)
+parseOne (x:r) | (isLower x) = (Terminal (Variable x), r)
 
--- Application
+-- Syntax error
 
--- parse (x:e)
+-- parseOne (s) = trace ("Syntax error at: " ++ s) (EmptyExpression, "")

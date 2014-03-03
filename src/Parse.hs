7be22ae264dc27473c6c0c14866b47034826b61f
Juan Pineda <juan@logician.com>
2014-03-03 07:13:35 +0000

    Break out Evaluate, Parse, and Test

---
+++ src/Parse.hs
@@ -0,0 +1,82 @@
+module Parse where
+
+import           Data.Char
+import qualified Data.Map  as Map
+
+-- Literal definitions
+
+identity = "Lz.z"
+zero = "Ls.Lz.z"
+one  = "Ls.Lz.sz"
+two  = "Ls.Lz.s(sz)"
+successor = "Lw.Ly.Lx.y(wyx)"
+predecessor = "Ln.Lf.Lx.n(Lg.Lh.h(gf))(Lu.x)(Lu.u)"
+
+-- The syntax tree
+
+data Expr =
+      Var Char
+    | Lambda Char Expr
+    | Apply Expr Expr
+    | Empty
+    | Unbound Char
+    | Bound Char
+    | Closure Char Expr (Map.Map Char Expr)
+  deriving (Show, Eq)
+
+-- Parser
+
+parse :: String -> Expr
+parse s
+    | s == "" = error ("parse: syntax error: empty string")
+    | otherwise = fst (parseApply t r)
+    where (t,r) = parseTerm s
+
+-- Left associative expression application
+
+parseApply :: Expr -> String -> (Expr, String)
+
+parseApply acc s
+    | s == "" = {- trace "parseLeft: EOS" -} (acc, "")
+    | head s == ')' = (acc, s)
+    | acc == Empty = (parseApply t r)
+    | otherwise = {- trace "parseLeft: Apply" -} (parseApply (Apply acc t) r)
+    where (t,r) = parseTerm s
+
+-- Parse a term
+
+parseTerm :: String -> (Expr, String)
+
+-- Parens:
+
+parseTerm ('(':s)
+    | r == "" = error ("parseTerm: empty string after left paren " ++ show e)
+    | head r == ')' = (e, tail r)
+    | otherwise = error ("parseTerm: missing right paren: " ++ r)
+    where (e,r) = parseApply Empty s
+
+--    | r /= "" && (head r) == ')' = ((parse e) , tail r)
+--    where (e,r) = break (== ')') s
+--    error ("parseTerm: e,r = " ++ e ++ " " ++ r)
+
+-- Lambda:
+
+parseTerm ('L':x:'.':e) | (isLower x) =
+    let (exp, r) = parseApply Empty e in (Lambda x exp, r)
+
+-- Variable:
+
+parseTerm (x:r) | (isLower x) = (Var x, r)
+
+-- Literals :
+
+parseTerm (x:r) | x == 'I' = (parse(identity), r)
+parseTerm (x:r) | x == '0' = (parse(zero), r)
+parseTerm (x:r) | x == '1' = (parse(one), r)
+parseTerm (x:r) | x == '2' = (parse(two), r)
+parseTerm (x:r) | x == 'S' = (parse(successor), r)
+parseTerm (x:r) | x == 'P' = (parse(predecessor), r)
+
+-- Syntax error:
+
+parseTerm(s) = error ("parseTerm: syntax error: " ++ s)
